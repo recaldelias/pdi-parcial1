8b7e514d42a1e77b7a242d1655b5d86ffdce6bd3
recaldelias <61387750+recaldelias@users.noreply.github.com>
2022-09-16 10:02:15 -0400

    se arreglan problemas varios

--- QHELC_IR.m
+++ QHELC_IR.m
@@ -1,64 +1,63 @@
-function image = QHELC_IR(f,gamma)
-    
+function image = QHELC_IR(f,gamma)  %                               PASO 1
+%                                                                   PASO 2
     [count,binLocation]=imhist(f);
-    histo=[binLocation,count];
+    histo=[binLocation, count ];
+    pK = zeros(256,2);
+    [x,y]=size(f);
     
-    [cantidad, clasificacion] = histcounts(f(:), 'Normalization', 'probability','NumBins',256 );
-    clasificacion(end)=[];
-    figure; plot(count, binLocation);
-    figure; plot( binLocation, count);
-%    SE HALLA EL PRIMER THRESHOLD
+    for i=1:256
+        pK(i,1)=i-1;
+        pK(i,2)=histo(i,2)/(x*y);
+    end
+    
+%    SE HACE LA PRIMERA DIVISION                                    PASO 3
 
-    transpuesto=transpose([clasificacion;cantidad]);
-    [m,n]=size(transpuesto);
+    
+    [m,~]=size(histo);
     promedio=average(histo,1,m);
     centroide=inicializarCentroide(promedio);
-    [test,c,sum,D]=kmeans(transpuesto,2,'start',centroide);
+    %[test,c,sum,D]=kmeans(histo,2,'start',centroide);
+    [test,~,~,~]=kmeans(pK,2,'start',centroide);
     SP=spFinder(test);
-        
-
-%   SE APLICA KMEANS A LA PRIMERA MITAD
+    
+%   SE APLICA KMEANS SOBRE LA PRIMERA MITAD                         PASO 4
+    
     promedio1=average(histo,1,SP);
     centroide=inicializarCentroide(promedio1);
-    [test1,c1,sum1,D1]=kmeans(transpuesto(1:SP,:),2,'start',centroide);
+    [test1,~,~,~]=kmeans(pK(1:SP,:),2,'start',centroide);
     SPL=spFinder(test1);
-    
-    
-% SE APLICA KMEANS A LA SEGUNDA MITAD
+
+%   SE APLICA KMEANS A LA SEGUNDA MITAD                             PASO 5 
     promedio2=average(histo,SP+1,m);
     centroide=inicializarCentroide(promedio2);
-    [test2,c2,sum2,D2]=kmeans(transpuesto(SP+1:m,:),2,'start',centroide);
-    SPU=spFinder(test2)+SP;
-    
-    
-% SE HALLAN LOS CUTOFF LIMITS
+    [test2,~,~,~]=kmeans(pK(SP+1:m,:),2,'start',centroide);
+    SPU=spFinder(test2)+SP;    
+
+%   SE HALLAN LOS CUTOFF LIMITS                                     PASO 5.1
 
     CL1 = cutOffLimit(histo, 1, SPL, gamma);
     CL2 = cutOffLimit(histo, SPL+1, SP, gamma);
     CU1 = cutOffLimit(histo, SP+1, SPU, gamma);
     CU2 = cutOffLimit(histo, SPU+1, 256, gamma);
 
-%  SE CALCULAN LOS PIXELES QUE ESTAN ENCIMA DE LOS CUTOFF POINTS
+% SE CALCULAN LOS PIXELES QUE ESTAN ENCIMA DE LOS CUTOFF POINTS    PASO 5.2
 
-    TL1 = aboveCutOff(histo, 1, SPL, CL1);
-    TL2 = aboveCutOff(histo, SPL+1 , SP, CL2);
-    TU1 = aboveCutOff(histo, SP+1, SPU, CU1);
-    TU2 = aboveCutOff(histo, SPU+1, 256, CU2);
-    
-    
-%   SE CALCULA EL AVERAGE INCREMENT
+    TI = aboveCutOff(histo, SP, SPL, SPU, CL1, CL2, CU1, CU2);
 
-    AL1 = averageIncrement(TL1, 1, SPL);
-    AL2 = averageIncrement(TL2, SPL+1, SP);
-    AU1 = averageIncrement(TU1, SP+1, SPU);
-    AU2 = averageIncrement(TU2, SPU+1, 256);
     
-%   SE SACAN LOS PIXELES QUE ESTAN ENCIMA DE LOS CUTOFF POINTS
     
-    hmod = trimmedH(histo, 1, SPL, CL1, AL1);
-    hmod = trimmedH(hmod, SPL+1, SP, CL2, AL2);
-    hmod = trimmedH(hmod, SP+1, SPU, CU1, AU1);
-    hmod = trimmedH(hmod, SPU+1, 256, CU2, AU2);
+%   SE CALCULA EL AVERAGE INCREMENT                                 PASO 5.3
+
+    AI = averageIncrement( TI, SP, SPL, SPU);
+    
+    %[AI,TI]
+    
+%   SE SACAN LOS PIXELES QUE ESTAN ENCIMA DE LOS CUTOFF POINTS      PASO 6
+    
+    hmod = trimmedH(histo, 1, SPL, CL1, AI);
+    hmod = trimmedH(hmod, SPL+1, SP, CL2, AI);
+    hmod = trimmedH(hmod, SP+1, SPU, CU1, AI);
+    hmod = trimmedH(hmod, SPU+1, 256, CU2, AI);
     
 
     
@@ -67,12 +66,13 @@ function image = QHELC_IR(f,gamma)
     equalizado= equalizacion(hmod, SP, SPL, SPU);
     
 
-%   MODIFICACION DE IMAGEN
+%   MODIFICACION DE IMAGEN                                          PASO 7
 
-    [m,n]=size(f);
+%   se modifican los niveles de gris por los hallados en el vector
+%   equalizacion
     image=f;
-    for i=1:m
-        for j=1:n
+    for i=1:x
+        for j=1:y
             if equalizado(image(i,j)+1,2)~=0
                 image(i,j)=equalizado(image(i,j)+1,2);
             end
@@ -86,6 +86,7 @@ function image = QHELC_IR(f,gamma)
 end
     
 function promedio= average(histograma,inicio,fin)
+%   funcion que halla el promedio de un histograma
     x=0;
     y=0;
     for i=inicio:fin
@@ -97,6 +98,8 @@ function promedio= average(histograma,inicio,fin)
 end
 
 function centroide= inicializarCentroide(promedio)
+%   funcion que inicializa la matriz de centroides para utilizar en
+%   en algoritmo de kmeans
     matriz=zeros(2,2);
 
     for i=1:2
@@ -107,6 +110,7 @@ function centroide= inicializarCentroide(promedio)
 end
 
 function SP= spFinder(test)
+%   funcion para hallar las divisiones entre los clusters
     i=1;
     while test(i)==test(1)
         SP=i;
@@ -115,6 +119,7 @@ function SP= spFinder(test)
 end
 
 function CI = cutOffLimit(histo,LL,UP,gamma)
+%   Establece los limites del histograma
     N=0;
     for i=LL:UP
         N= histo(i,2 ) +N;
@@ -125,26 +130,66 @@ function CI = cutOffLimit(histo,LL,UP,gamma)
 
 end
 
-function T = aboveCutOff(histo,LL,UP,CL)
-T=0;    
-    for i=LL:UP
-        T = T + max(histo(i,2)-CL,0);
+function T = aboveCutOff(histo, SP, SPL, SPU, CL1, CL2, CU1, CU2)
+%   Encuentra la cantidad de pixeles sobre el cutoff limit
+    T=zeros(256,1);    
+    for i=1:256
+        if i>=0 && i<=SPL
+
+            T(i,1) = max(histo(i,2)-CL1,0);
+        elseif i>=(SPL+1) && i<=SP
+
+            T(i,1) = max(histo(i,2)-CL2,0);
+        elseif i>=(SP+1) && i<=SPU
+            
+            T(i,1) = max(histo(i,2)-CU1,0);
+        elseif i>=(SPU+1) && i<=256
+            
+            T(i,1) = max(histo(i,2)-CU2,0);
+        end
     end
 end
 
-function A = averageIncrement(T,   LL , UP)
-    I = UP - LL;
-    A = floor(T/I);
+
+
+
+function A = averageIncrement( TI, SP, SPL, SPU)
+%   calcula el incremento promedio de la intensidad de los distintos
+%   niveles de gris
+    A=zeros(256,1);
+
+    for i=1:256
+        if i>=0 && i<=SPL
+            
+            longitud = SPL;
+            A(i,1) = floor(TI(i,1)/longitud);
+        elseif i>=(SPL+1) && i<=SP
+
+            longitud = SP -SPL -1;
+            A(i,1) = floor(TI(i,1)/longitud);
+        elseif i>=(SP+1) && i<=SPU
+            
+            longitud = SPU -SP -1;
+            A(i,1) = floor(TI(i,1)/longitud);
+        elseif i>=(SPU+1) && i<=256
+            
+            longitud = 255 -SPU -1;
+            A(i,1) = floor(TI(i,1)/longitud);
+        end
+    end
 end
 
 
 function h = trimmedH (histo, LL, UP, CL, AI)
+%   funcion que modifica los distintos histogramas retirando los pixeles
+%   que esten encima de la barrera del cutoff limit
     for i = LL:UP
-        if histo(i,2) > (CL-AI)
+        if histo(i,2) > (CL-AI(i,1))
             histo(i,2) = CL;
             
         else
-            histo(i,2)= histo(i,2)+AI;
+            histo(i,2)= histo(i,2)+AI(i,1);
+
         end
     end
     h=histo;
@@ -152,6 +197,7 @@ end
 
 
 function e = equalizacion(hmod, SP, SPL, SPU)
+%   funcion que adapta el histograma para modificar la imagen
     e=zeros(256,2);
     for i=1:256
         e(i,1) = i-1;
@@ -178,6 +224,7 @@ end
 
 
 function a= acumulativo(q, histo, LL, UP)
+%   funcion C(q) en el paper
     a=0;
     for i=LL:q
         a= a + pdf(i, histo, LL, UP);
@@ -185,6 +232,7 @@ function a= acumulativo(q, histo, LL, UP)
 end
 
 function p=pdf(q, histo, LL, UP)
+% funcion que halla el probability density function de un histograma
     N=0;
     
     for i=LL:UP
@@ -196,13 +244,15 @@ function p=pdf(q, histo, LL, UP)
 end
 
 function  graficarHistograma(histo, SP, SPL, SPU, CL1, CL2, CU1, CU2, titulo)
+%   funcion para graficar el histograma
     
-    plot(histo)
-    line([SP SP],[0 3000],'color','red')
+    plot(histo(:,1),histo(:,2))
+    %line([SP SP],[0 3000],'color','red')
+    xline(SP,'color','red')
     text(SP,0,'SP')
-    line([SPL SPL],[0 3000],'color','green')
+    xline(SPL,'color','green')
     text(SPL,0,'SPL')
-    line([SPU SPU],[0 3000],'color','blue')
+    xline(SPU,'color','blue')
     text(SPU,0,'SPU')
     text(256,0,'L')
     line([0 SPL],[CL1 CL1],'color','magenta')
